4c277162a081d660b85fe1f50376b4bed5107fe8
Liam Messinger <liam.messinger@gmail.com>
2025-09-16 20:30:44 -0400

    Added image creation through "image" dependancy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use image::{ImageBuffer, Rgb};
+
 fn main() {
-    println!("Hello, world!");
+
+    // Image
+
+    let IMAGE_WIDTH: u32 = 265;
+    let IMAGE_HEIGHT: u32 = 265;
+
+    // Create image buffer
+    let mut img = ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    // Render
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", j);
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            // Set pixel in image buffer
+            img.put_pixel(i, j, Rgb([ir, ig, ib]));
+        }
+    }
+    
+    // Save the image
+    img.save("output.png").unwrap();
+    eprintln!("Done. Image saved as output.png");
 }
