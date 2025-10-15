c0b3e57925a7efb9eeedb2d2cd2a0d286b14a2a2
Liam Messinger <liam.messinger@gmail.com>
2025-10-15 11:56:13 -0400

    Refactor material handling in main function to remove Material wrapper

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ mod camera;
 mod material;
 
 use prelude::*;
-use crate::hittable::{HittableList, HittableObject, Sphere};
+use crate::hittable::{HittableList, Sphere};
 use crate::camera::Camera;
 use crate::material::{Material, Lambertian, Metal, Dielectric};
 
 fn main() {
     let mut world = HittableList::new();
 
-    let ground_material = Material::Lambertian(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    let ground_material = Lambertian::new(Color::new(0.5, 0.5, 0.5));
     world.add(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material));
 
     for a in -11..11 {
@@ -26,35 +26,33 @@ fn main() {
             let center = Point3::new(a as f64 + 0.9 * random_f64(), 0.2, b as f64 + 0.9 * random_f64());
 
             if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                let sphere_material: Material;
-
                 if choose_mat < 0.8 {
                     // diffuse
                     let albedo = Color::random() * Color::random();
-                    sphere_material = Material::Lambertian(Lambertian::new(albedo));
+                    let sphere_material = Lambertian::new(albedo);
                     world.add(Sphere::new(center, 0.2, sphere_material));
                 } else if choose_mat < 0.95 {
                     // metal
                     let albedo = Color::random_range(0.5, 1.0);
                     let fuzz = random_f64_range(0.0, 0.5);
-                    sphere_material = Material::Metal(Metal::new(albedo, fuzz));
+                    let sphere_material = Metal::new(albedo, fuzz);
                     world.add(Sphere::new(center, 0.2, sphere_material));
                 } else {
                     // glass
-                    sphere_material = Material::Dielectric(Dielectric::new(1.5));
+                    let sphere_material = Dielectric::new(1.5);
                     world.add(Sphere::new(center, 0.2, sphere_material));
                 }
             }
         }
     }
 
-    let material1 = Material::Dielectric(Dielectric::new(1.5));
+    let material1 = Dielectric::new(1.5);
     world.add(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, material1));
 
-    let material2 = Material::Lambertian(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    let material2 = Lambertian::new(Color::new(0.4, 0.2, 0.1));
     world.add(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, material2));
 
-    let material3 = Material::Metal(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    let material3 = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
     world.add(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, material3));
 
     let mut cam = Camera::default();
