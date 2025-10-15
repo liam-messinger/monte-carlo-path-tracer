beb66acdb47a0c405074224156448f32b5659ef1
Liam Messinger <liam.messinger@gmail.com>
2025-10-15 10:00:42 -0400

    Refactor main to use Material directly instead of Rc<dyn Material>

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ mod material;
 use prelude::*;
 use crate::hittable::{HittableList, Sphere};
 use crate::camera::Camera;
-use crate::material::{Lambertian, Metal, Dielectric};
+use crate::material::{Material, Lambertian, Metal, Dielectric};
 use std::rc::Rc;
 
 fn main() {
     let mut world = HittableList::new();
 
-    let ground_material = make_shared!(Lambertian, Color::new(0.5, 0.5, 0.5));
-    world.add(make_shared!(Sphere, Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material));
+    let ground_material = Material::Lambertian(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material));
 
     for a in -11..11 {
         for b in -11..11 {
@@ -27,36 +27,36 @@ fn main() {
             let center = Point3::new(a as f64 + 0.9 * random_f64(), 0.2, b as f64 + 0.9 * random_f64());
 
             if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                let sphere_material: Rc<dyn material::Material>;
+                let sphere_material: Material;
 
                 if choose_mat < 0.8 {
                     // diffuse
                     let albedo = Color::random() * Color::random();
-                    sphere_material = make_shared!(Lambertian, albedo);
-                    world.add(make_shared!(Sphere, center, 0.2, sphere_material));
+                    sphere_material = Material::Lambertian(Lambertian::new(albedo));
+                    world.add(Sphere::new(center, 0.2, sphere_material));
                 } else if choose_mat < 0.95 {
                     // metal
                     let albedo = Color::random_range(0.5, 1.0);
                     let fuzz = random_f64_range(0.0, 0.5);
-                    sphere_material = make_shared!(Metal, albedo, fuzz);
-                    world.add(make_shared!(Sphere, center, 0.2, sphere_material));
+                    sphere_material = Material::Metal(Metal::new(albedo, fuzz));
+                    world.add(Sphere::new(center, 0.2, sphere_material));
                 } else {
                     // glass
-                    sphere_material = make_shared!(Dielectric, 1.5);
-                    world.add(make_shared!(Sphere, center, 0.2, sphere_material));
+                    sphere_material = Material::Dielectric(Dielectric::new(1.5));
+                    world.add(Sphere::new(center, 0.2, sphere_material));
                 }
             }
         }
     }
 
-    let material1 = make_shared!(Dielectric, 1.5);
-    world.add(make_shared!(Sphere, Point3::new(0.0, 1.0, 0.0), 1.0, material1));
+    let material1 = Material::Dielectric(Dielectric::new(1.5));
+    world.add(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, material1));
 
-    let material2 = make_shared!(Lambertian, Color::new(0.4, 0.2, 0.1));
-    world.add(make_shared!(Sphere, Point3::new(-4.0, 1.0, 0.0), 1.0, material2));
+    let material2 = Material::Lambertian(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, material2));
 
-    let material3 = make_shared!(Metal, Color::new(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared!(Sphere, Point3::new(4.0, 1.0, 0.0), 1.0, material3));
+    let material3 = Material::Metal(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, material3));
 
     let mut cam = Camera::default();
 
