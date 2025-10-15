b02fb6a363b8c019307e17d91a6f0ac39979bbdb
Liam Messinger <liam.messinger@gmail.com>
2025-10-15 14:41:06 -0400

    Add inline annotations to Vec3 methods for improved performance

--- src/vec3.rs
+++ src/vec3.rs
@@ -23,12 +23,15 @@ impl Vec3 {
     }
 
     // Accessors
+    #[inline]
     pub fn x(&self) -> f64 {
         self.e[0]
     }
+    #[inline]
     pub fn y(&self) -> f64 {
         self.e[1]
     }
+    #[inline]
     pub fn z(&self) -> f64 {
         self.e[2]
     }
@@ -36,27 +39,32 @@ impl Vec3 {
     // ----------------- Utility functions -----------------
 
     // vec.length()
+    #[inline]
     pub fn length(&self) -> f64 { // length of the vector
         self.length_squared().sqrt()
     }
 
     // vec.length_squared()
+    #[inline]
     pub fn length_squared(&self) -> f64 { // squared length of the vector
         self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
 
     // vec.near_zero()
+    #[inline]
     pub fn near_zero(&self) -> bool { // checks if the vector is close to zero in all dimensions
         let s = 1e-8;
         self.e[0].abs() < s && self.e[1].abs() < s && self.e[2].abs() < s
     }
 
     // Vec3::random()
+    #[inline]
     pub fn random() -> Self { // random vector with each component in [0,1)
         Vec3::new(random_f64(), random_f64(), random_f64())
     }
 
     // Vec3::random_range(min, max)
+    #[inline]
     pub fn random_range(min: f64, max: f64) -> Self { // random vector with each component in [min,max)
         Vec3::new( 
             random_f64() * (max - min) + min,
@@ -66,11 +74,13 @@ impl Vec3 {
     }
 
     // Vec3::dot(u, v)
+    #[inline]
     pub fn dot(u: &Vec3, v: &Vec3) -> f64 { // dot product of two vectors
         u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
     }
 
     // Vec3::cross(u, v)
+    #[inline]
     pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 { // cross product of two vectors
         Vec3 {
             e: [
@@ -82,11 +92,13 @@ impl Vec3 {
     }
 
     // Vec3::unit_vector(v)
+    #[inline]
     pub fn unit_vector(v: Vec3) -> Vec3 { // returns the unit vector in the direction of v
         v / v.length()
     }
 
     // Vec3::random_in_unit_circle()
+    #[inline]
     pub fn random_in_unit_circle() -> Vec3 { // Generates a random point in the unit disk in the XY plane
         loop {
             let p = Vec3::new(random_f64_range(-1.0, 1.0), random_f64_range(-1.0, 1.0), 0.0);
@@ -97,6 +109,7 @@ impl Vec3 {
     }
 
     // Vec3::random_unit_vector()
+    #[inline]
     pub fn random_unit_vector() -> Vec3 { // Generates a random unit vector uniformly distributed over the unit sphere
         loop {
             let v = Vec3::random_range(-1.0, 1.0);
@@ -108,6 +121,7 @@ impl Vec3 {
     }
 
     // Vec3::random_in_hemisphere(normal)
+    #[inline]
     pub fn random_in_hemisphere(normal: &Vec3) -> Vec3 { // Using a normal, generates a random vector in the "same" direction
         let on_unit_sphere = Vec3::random_unit_vector();
         if Vec3::dot(&on_unit_sphere, normal) > 0.0 {
@@ -118,11 +132,13 @@ impl Vec3 {
     }
 
     // Vec3::reflect(v, n)
+    #[inline]
     pub fn reflect(vec: &Vec3, normal: &Vec3) -> Vec3 { // Reflects vector vec around a normal
         (*vec) - 2.0 * Vec3::dot(vec, normal) * (*normal)
     }
 
     // Vec3::refract(ray_in, normal, etai_over_etat)
+    #[inline]
     pub fn refract(ray_in: &Vec3, normal: &Vec3, etai_over_etat: f64) -> Vec3 { // Refracts ray_in with normal and ratio of indices of refraction
         let cos_theta = f64::min(Vec3::dot(&-*ray_in, normal), 1.0);
         let r_out_perp = etai_over_etat * (*ray_in + cos_theta * (*normal));
@@ -136,6 +152,7 @@ impl Vec3 {
 impl Neg for Vec3 {
     // -v
     type Output = Vec3;
+    #[inline]
     fn neg(self) -> Vec3 {
         Vec3 {
             e: [-self.e[0], -self.e[1], -self.e[2]],
@@ -146,6 +163,7 @@ impl Neg for Vec3 {
 impl Index<usize> for Vec3 {
     // v[i]
     type Output = f64;
+    #[inline]
     fn index(&self, i: usize) -> &f64 {
         &self.e[i]
     }
@@ -153,6 +171,7 @@ impl Index<usize> for Vec3 {
 
 impl AddAssign for Vec3 {
     // v += u
+    #[inline]
     fn add_assign(&mut self, other: Vec3) {
         self.e[0] += other.e[0];
         self.e[1] += other.e[1];
@@ -162,6 +181,7 @@ impl AddAssign for Vec3 {
 
 impl MulAssign<f64> for Vec3 {
     // v *= t
+    #[inline]
     fn mul_assign(&mut self, t: f64) {
         self.e[0] *= t;
         self.e[1] *= t;
@@ -171,6 +191,7 @@ impl MulAssign<f64> for Vec3 {
 
 impl DivAssign<f64> for Vec3 {
     // v /= t
+    #[inline]
     fn div_assign(&mut self, t: f64) {
         let inv_t = 1.0 / t;
         self.e[0] *= inv_t;
@@ -182,6 +203,7 @@ impl DivAssign<f64> for Vec3 {
 impl Add for Vec3 {
     // v + u
     type Output = Vec3;
+    #[inline]
     fn add(self, other: Vec3) -> Vec3 {
         Vec3 {
             e: [
@@ -196,6 +218,7 @@ impl Add for Vec3 {
 impl Sub for Vec3 {
     // v - u
     type Output = Vec3;
+    #[inline]
     fn sub(self, other: Vec3) -> Vec3 {
         Vec3 {
             e: [
@@ -210,6 +233,7 @@ impl Sub for Vec3 {
 impl Mul for Vec3 {
     // v * u (element-wise multiplication)
     type Output = Vec3;
+    #[inline]
     fn mul(self, other: Vec3) -> Vec3 {
         Vec3 {
             e: [ 
@@ -224,6 +248,7 @@ impl Mul for Vec3 {
 impl Mul<f64> for Vec3 {
     // v * t
     type Output = Vec3;
+    #[inline]
     fn mul(self, t: f64) -> Vec3 {
         Vec3 {
             e: [self.e[0] * t, self.e[1] * t, self.e[2] * t],
@@ -234,6 +259,7 @@ impl Mul<f64> for Vec3 {
 impl Mul<Vec3> for f64 {
     // t * v
     type Output = Vec3;
+    #[inline]
     fn mul(self, v: Vec3) -> Vec3 {
         Vec3 {
             e: [v.e[0] * self, v.e[1] * self, v.e[2] * self],
@@ -244,6 +270,7 @@ impl Mul<Vec3> for f64 {
 impl Div<f64> for Vec3 {
     // v / t
     type Output = Vec3;
+    #[inline]
     fn div(self, t: f64) -> Vec3 {
         let inv_t = 1.0 / t;
         Vec3 {
