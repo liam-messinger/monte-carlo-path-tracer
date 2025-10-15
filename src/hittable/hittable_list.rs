eab0187b7518f72d29301afa0739d13cd94980de
Liam Messinger <liam.messinger@gmail.com>
2025-10-15 10:16:49 -0400

    Refactor HittableList to use HittableObject instead of Rc<dyn Hittable>

--- src/hittable/hittable_list.rs
+++ src/hittable/hittable_list.rs
@@ -1,31 +1,32 @@
-use super::core::{HitRecord, Hittable};
+use super::core::{HitRecord, HittableObject};
 use crate::ray::Ray;
 use crate::interval::Interval;
 use std::rc::Rc;
 
 pub struct HittableList {
-    pub objects: Vec<Rc<dyn Hittable>>,
+    pub objects: Vec<HittableObject>,
 }
 
 impl HittableList {
     // Constructor for HittableList
     pub fn new() -> Self {
-        Self { objects: Vec::new() }
-    }
-
-    // Add an object to the list
-    pub fn add(&mut self, object: Rc<dyn Hittable>) {
-        self.objects.push(object);
+        Self { 
+            objects: Vec::new()
+        }
     }
 
     // Clear all objects from the list
     pub fn clear(&mut self) {
         self.objects.clear();
     }
-}
 
-impl Hittable for HittableList {
-    fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
+    // Add an object to the list
+    pub fn add(&mut self, object: HittableObject) {
+        self.objects.push(object);
+    }
+
+    // Check for ray intersections with all objects in the list
+    pub fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
         let mut temp_rec = HitRecord::new();
         let mut hit_anything = false;
         let mut closest_so_far = ray_t.max;
