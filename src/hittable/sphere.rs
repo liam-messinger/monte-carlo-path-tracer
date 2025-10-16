b4d2bb2a0e3b5de676fc5ff9ef9fceab999019e6
Liam Messinger <liam.messinger@gmail.com>
2025-10-16 17:26:18 -0400

    Refactor Sphere struct to use Ray for center and adding moving sphere

--- src/hittable/sphere.rs
+++ src/hittable/sphere.rs
@@ -7,16 +7,25 @@ use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct Sphere {
-    pub center: Point3,
+    pub center: Ray,
     pub radius: f64,
     pub material: Arc<Material>,
 }
 
 impl Sphere {
-    // Constructor for Sphere
+    // Constructor for Stationary Sphere
     pub fn new(center: Point3, radius: f64, material: impl Into<Material>) -> Self {
         Self {
-            center,
+            center: Ray::new(center, Vec3::zero()),
+            radius: radius.max(0.0),
+            material: Arc::new(material.into()),
+        }
+    }
+
+    // Constructor for Moving Sphere
+    pub fn new_moving(center1: Point3, center2: Point3, radius: f64, material: impl Into<Material>) -> Self {
+        Self {
+            center: Ray::new(center1, center2 - center1),
             radius: radius.max(0.0),
             material: Arc::new(material.into()),
         }
@@ -25,8 +34,9 @@ impl Sphere {
     // Check for ray-sphere intersection
     #[inline]
     pub fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
+        let current_center: Point3 = self.center.at(r.time);
         // Calculate the discriminant of the quadratic equation for ray-sphere intersection
-        let oc: Vec3 = self.center - r.origin;
+        let oc: Vec3 = current_center - r.origin;
         let a = r.direction.length_squared();
         let h = Vec3::dot(&r.direction, &oc);
         let c = oc.length_squared() - self.radius * self.radius;
@@ -48,7 +58,7 @@ impl Sphere {
 
         rec.t = root;
         rec.point = r.at(rec.t);
-        let outward_normal = (rec.point - self.center) / self.radius;
+        let outward_normal = (rec.point - current_center) / self.radius;
         rec.set_face_normal(r, &outward_normal);
         rec.material = Arc::clone(&self.material);
 
