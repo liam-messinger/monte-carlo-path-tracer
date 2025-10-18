4cea03387f8baead62111130d10c54e019fb1129
Liam Messinger <liam.messinger@gmail.com>
2025-10-18 17:39:10 -0400

    Refactor Hittable enum to include HittableList and BVHNode variants

--- src/hittable/core.rs
+++ src/hittable/core.rs
@@ -3,6 +3,11 @@ use crate::vec3::{Point3, Vec3};
 use crate::interval::Interval;
 use crate::material::{Material};
 use crate::hittable::Sphere;
+use crate::aabb::AABB;
+
+use super::hittable_list::HittableList;
+use super::bvh_node::BVHNode;
+
 use std::sync::Arc;
 
 // Record of a ray-object intersection
@@ -39,18 +44,32 @@ impl HitRecord {
     }
 }
 
+// TODO: Move Hittable enum to its own file
 // ----- Enum for Hittable Object Types -----
 #[derive(Clone)]
-pub enum HittableObject {
+pub enum Hittable {
+    HittableList(HittableList),
+    BVHNode(BVHNode),
     Sphere(Sphere),
     // Etc.
 }
 
-impl HittableObject {
+impl Hittable {
     #[inline]
-    pub fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
+    pub fn hit(&self, r: &Ray, ray_t: &Interval, rec: &mut HitRecord) -> bool {
         match self {
-            HittableObject::Sphere(sphere) => sphere.hit(r, ray_t, rec),
+            Hittable::HittableList(list) => list.hit(r, ray_t, rec),
+            Hittable::BVHNode(node) => node.hit(r, ray_t, rec),
+            Hittable::Sphere(sphere) => sphere.hit(r, ray_t, rec),
+            // Etc.
+        }
+    }
+
+    pub fn bounding_box(&self) -> &AABB {
+        match self {
+            Hittable::HittableList(list) => list.bounding_box(),
+            Hittable::BVHNode(node) => node.bounding_box(),
+            Hittable::Sphere(sphere) => sphere.bounding_box(),
             // Etc.
         }
     }
