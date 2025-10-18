b014191617efa574c5fb2520a34f2045d5e80b51
Liam Messinger <liam.messinger@gmail.com>
2025-10-18 17:37:00 -0400

    Refactor HittableList to use Arc<Hittable> and add bounding box management

--- src/hittable/hittable_list.rs
+++ src/hittable/hittable_list.rs
@@ -1,17 +1,37 @@
-use super::core::{HitRecord, HittableObject};
+use super::core::{HitRecord, Hittable};
+use super::bvh_node::BVHNode;
 use crate::ray::Ray;
 use crate::interval::Interval;
+use crate::aabb::AABB;
 
+use std::sync::Arc;
+
+#[derive(Clone)]
 pub struct HittableList {
-    pub objects: Vec<HittableObject>,
+    pub objects: Vec<Arc<Hittable>>,
+    bounding_box: AABB,
 }
 
 impl HittableList {
     // Constructor for HittableList
     pub fn new() -> Self {
         Self { 
-            objects: Vec::new()
+            objects: Vec::new(),
+            bounding_box: AABB::empty(),
         }
+    }
+
+    pub fn from_hittable(object: impl Into<Hittable>) -> Self {
+        let hittable_object = object.into();
+        let bounding_box = hittable_object.bounding_box().clone();
+        Self {
+            objects: vec![Arc::new(hittable_object)],
+            bounding_box,
+        }
+    }
+
+    pub fn to_bvh(&mut self) -> BVHNode {
+        BVHNode::build_from_list(self)
     }
 
     // Clear all objects from the list
@@ -20,18 +40,28 @@ impl HittableList {
     }
 
     // Add an object to the list
-    pub fn add(&mut self, object: impl Into<HittableObject>) {
-        self.objects.push(object.into());
+    pub fn add(&mut self, object: impl Into<Hittable>) {
+        let hittable_object = object.into();
+        self.bounding_box = AABB::merge(&self.bounding_box, hittable_object.bounding_box());
+        self.objects.push(Arc::new(hittable_object));
     }
 
     // Check for ray intersections with all objects in the list
-    pub fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
+    // TODO: Remove cloning of HitRecord
+    // TODO: Remove repeated new Interval creation
+    #[inline]
+    pub fn hit(&self, r: &Ray, ray_t: &Interval, rec: &mut HitRecord) -> bool {
         let mut temp_rec = HitRecord::new();
         let mut hit_anything = false;
         let mut closest_so_far = ray_t.max;
 
+        // Reusable interval
+        let mut range = Interval::new(ray_t.min, closest_so_far);
+
         for object in &self.objects {
-            if object.hit(r, Interval::new(ray_t.min, closest_so_far), &mut temp_rec) {
+            range.max = closest_so_far;
+
+            if object.hit(r, &range, &mut temp_rec) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
                 *rec = temp_rec.clone();
@@ -39,5 +69,19 @@ impl HittableList {
         }
 
         hit_anything
+    }
+
+    // Get bounding box of the hittable list
+    pub fn bounding_box(&self) -> &AABB {
+        &self.bounding_box
+    }
+}
+
+
+
+// From HittableList to HittableObject conversion
+impl From<HittableList> for Hittable {
+    fn from(list: HittableList) -> Self {
+        Hittable::HittableList(list)
     }
 }
