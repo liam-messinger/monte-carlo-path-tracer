8aacfd1516c7a64c8b1768cb994013d90ca5a94c
Liam Messinger <liam.messinger@gmail.com>
2025-10-20 22:59:32 -0400

    Implement checkered spheres scene

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,30 @@ fn bouncing_spheres() {
     cam.render(world);
 }
 
+fn checkered_spheres() {
+    let mut world = HittableList::new();
+
+    let checker = CheckerTexture::from_colors(0.32, Color::new(0.2, 0.3, 0.1), Color::new(0.9, 0.9, 0.9));
+    world.add(Sphere::new(Point3::new(0.0, -10.0, 0.0), 10.0, Lambertian::from_texture(checker.clone().into())));
+    world.add(Sphere::new(Point3::new(0.0, 10.0, 0.0), 10.0, Lambertian::from_texture(checker.into())));
+
+    let mut cam = Camera::default();
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.v_fov = 20.0;
+    cam.look_from = Point3::new(13.0, 2.0, 3.0);
+    cam.look_at = Point3::new(0.0, 0.0, 0.0);
+    cam.v_up = Vec3::new(0.0, 1.0, 0.0);
+
+    cam.apature_angle = 0.0;
+
+    cam.render(world);
+}
+
 fn main() {
-    bouncing_spheres();
+    checkered_spheres();
 }
