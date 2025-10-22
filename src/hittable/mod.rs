5c3e62700f4413ed99e0ed0e2cfe205eb0b6b82e
Liam Messinger <liam.messinger@gmail.com>
2025-10-21 21:06:24 -0400

    Refactor module imports: update HitRecord and Hittable re-exports

--- src/hittable/mod.rs
+++ src/hittable/mod.rs
@@ -1,9 +1,12 @@
 // Re-exports
 pub mod sphere;
-pub mod core;
+pub mod hit_record;
+pub mod hittable;
 pub mod hittable_list;
 pub mod bvh_node;
 
 pub use sphere::Sphere;
-pub use core::HitRecord;
+pub use hit_record::HitRecord;
+pub use hittable::Hittable;
 pub use hittable_list::HittableList;
+pub use bvh_node::BVHNode;
