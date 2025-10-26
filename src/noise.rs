a5ad71ca4000b829fbea8a747697851f53bebd5e
Liam Messinger <liam.messinger@gmail.com>
2025-10-25 20:39:25 -0400

    Refactor Noise::value to use trilinear interpolation

--- src/noise.rs
+++ src/noise.rs
@@ -28,15 +28,28 @@ impl Noise {
     }
 
     pub fn value(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()).floor() as i32;
-        let j = (4.0 * p.y()).floor() as i32;
-        let k = (4.0 * p.z()).floor() as i32;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        self.randfloat[
-            self.perm_x[(i & 255) as usize] ^
-            self.perm_y[(j & 255) as usize] ^
-            self.perm_z[(k & 255) as usize]
-        ]
+        let i = (p.x()).floor() as i32;
+        let j = (p.y()).floor() as i32;
+        let k = (p.z()).floor() as i32;
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[
+                        self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]
+                    ];
+                }
+            }
+        }
+
+        Noise::trilinear_interp(&c, u, v, w)
     }
 
     fn perlin_generate_perm() -> [usize; POINT_COUNT] {
@@ -53,5 +66,20 @@ impl Noise {
             let target = random_usize_range(0, i);
             p.swap(i, target);
         }
+    }
+
+    fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
+                        * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
+                        * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 }
