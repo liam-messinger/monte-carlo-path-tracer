a68bf24ecf3afd690449c5f562f0e94587442b01
Liam Messinger <liam.messinger@gmail.com>
2025-11-20 13:20:35 -0500

    Refactor scene 4

--- src/main.rs
+++ src/main.rs
@@ -115,53 +115,43 @@ fn solar_system() {
     let mut world = HittableList::new();
 
     // Sun
-    let sun_tex: Arc<Texture> = ImageTexture::from_file("2k_sun.jpg").into();
-    let sun_mat: Arc<Material> = Lambertian::from_texture(sun_tex).into();
+    let sun_mat = Material::lambertian_tex(Texture::image("2k_sun.jpg"));
     world.add(Sphere::new(&Point3::new(-6.0, 0.0, 0.0), 2.2, sun_mat));
 
     // Mercury
-    let mercury_tex: Arc<Texture> = ImageTexture::from_file("2k_mercury.jpg").into();
-    let mercury_mat: Arc<Material> = Lambertian::from_texture(mercury_tex).into();
+    let mercury_mat = Material::lambertian_tex(Texture::image("2k_mercury.jpg"));
     world.add(Sphere::new(&Point3::new(-3.2, 0.05, -0.2), 0.15, mercury_mat));
 
     // Venus
-    let venus_tex: Arc<Texture> = ImageTexture::from_file("2k_venus_surface.jpg").into();
-    let venus_mat: Arc<Material> = Lambertian::from_texture(venus_tex).into();
+    let venus_mat = Material::lambertian_tex(Texture::image("2k_venus_surface.jpg"));
     world.add(Sphere::new(&Point3::new(-2.3, 0.07, 0.4), 0.24, venus_mat));
 
     // Earth
-    let earth_tex: Arc<Texture> = ImageTexture::from_file("2k_earth_daymap.jpg").into();
-    let earth_mat: Arc<Material> = Lambertian::from_texture(earth_tex).into();
+    let earth_mat = Material::lambertian_tex(Texture::image("2k_earth_daymap.jpg"));
     world.add(Sphere::new(&Point3::new(-1.4, 0.09, -0.35), 0.26, earth_mat));
 
     // Mars
-    let mars_tex: Arc<Texture> = ImageTexture::from_file("2k_mars.jpg").into();
-    let mars_mat: Arc<Material> = Lambertian::from_texture(mars_tex).into();
+    let mars_mat = Material::lambertian_tex(Texture::image("2k_mars.jpg"));
     world.add(Sphere::new(&Point3::new(-0.4, 0.06, 0.2), 0.18, mars_mat));
 
     // Jupiter
-    let jupiter_tex: Arc<Texture> = ImageTexture::from_file("2k_jupiter.jpg").into();
-    let jupiter_mat: Arc<Material> = Lambertian::from_texture(jupiter_tex).into();
+    let jupiter_mat = Material::lambertian_tex(Texture::image("2k_jupiter.jpg"));
     world.add(Sphere::new(&Point3::new(2.0, 0.2, 0.1), 0.95, jupiter_mat));
 
     // Saturn (rings not modeled; just a textured sphere)
-    let saturn_tex: Arc<Texture> = ImageTexture::from_file("2k_saturn.jpg").into();
-    let saturn_mat: Arc<Material> = Lambertian::from_texture(saturn_tex).into();
+    let saturn_mat = Material::lambertian_tex(Texture::image("2k_saturn.jpg"));
     world.add(Sphere::new(&Point3::new(4.6, 0.16, -0.25), 0.8, saturn_mat));
 
     // Uranus
-    let uranus_tex: Arc<Texture> = ImageTexture::from_file("2k_uranus.jpg").into();
-    let uranus_mat: Arc<Material> = Lambertian::from_texture(uranus_tex).into();
+    let uranus_mat = Material::lambertian_tex(Texture::image("2k_uranus.jpg"));
     world.add(Sphere::new(&Point3::new(6.7, 0.12, 0.35), 0.55, uranus_mat));
 
     // Neptune
-    let neptune_tex: Arc<Texture> = ImageTexture::from_file("2k_neptune.jpg").into();
-    let neptune_mat: Arc<Material> = Lambertian::from_texture(neptune_tex).into();
+    let neptune_mat = Material::lambertian_tex(Texture::image("2k_neptune.jpg"));
     world.add(Sphere::new(&Point3::new(8.6, 0.1, -0.1), 0.52, neptune_mat));
 
     // Stars
-    let star_texture: Arc<Texture> = ImageTexture::from_file("8k_stars_milky_way.jpg").into();
-    let star_material: Arc<Material> = Lambertian::from_texture(star_texture).into();
+    let star_material = Material::lambertian_tex(Texture::image("8k_stars_milky_way.jpg"));
     world.add(Sphere::new(&Point3::new(0.0, -30.0, -50.0), 50.0, star_material));
 
     // Camera tuned to frame the whole arc
