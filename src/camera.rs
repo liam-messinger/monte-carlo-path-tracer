a70b7e62fdb69fee4328da861cdea572c5c4752a
Liam Messinger <127435652+liam-messinger@users.noreply.github.com>
2026-03-04 15:41:12 -0500

    Hardcoded example to sample the light in the cornell box scene

--- src/camera.rs
+++ src/camera.rs
@@ -238,9 +238,27 @@ impl Camera {
             return emitted_color;
         }
 
-        // "What does the material physically say the scattering distribution should be in that direction?”
+        // ### Temp ###
+        let on_light = Vec3::new(random_f64_range(213.0,343.0), 554.0, random_f64_range(227.0,332.0));
+        let mut to_light = on_light - rec.point;
+        let distance_squared = to_light.length_squared();
+        to_light = Vec3::unit_vector(&to_light);
+
+        if Vec3::dot(&to_light, &rec.normal) < 0.0 { return emitted_color; }
+
+        let light_area = (343.0-213.0)*(332.0-227.0);
+        let light_cosine = to_light.y().abs();
+        if light_cosine < 0.000001 { return emitted_color; }
+
+        pdf_value = distance_squared / (light_cosine * light_area);
+        scattered = Ray::new_with_time(rec.point, to_light, r.time);
+
         let scattering_pdf = rec.material.scattering_pdf(r, rec, &scattered);
-        let pdf_value = scattering_pdf;
+        // ### Temp ###
+
+        // "What does the material physically say the scattering distribution should be in that direction?”
+        //let scattering_pdf = rec.material.scattering_pdf(r, rec, &scattered);
+        //let pdf_value = scattering_pdf;
         
         let scattered_color = (attenuation * scattering_pdf * self.ray_color(&scattered, depth - 1, world, rec)) / pdf_value;
 
